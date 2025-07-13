0f8059b39c2c6238e2a79f38dd382f239c9b8d5b
fox <textfox10000@gmail.com>
2025-07-14 00:26:16 +0300

    Problem 02

--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -7,16 +7,23 @@ namespace CSharp_Advanced
     {
         static void Main(string[] args)
         {
-            Employees emp = new Employees(
-    id: 569,
-    name: "Ibrahim Taalab",
-    securityLVL: SecurityLVL.Developer,
-    salary: 55000m,
-    hireDate: new DateTime(2025, 3, 1),
-    gender: Gender.F
-);
+            #region 1. Design and implement a Class for the employees in a company:
+            //Employees emp = new Employees(
+            //id: 569,
+            //name: "Ibrahim Taalab",
+            //securityLVL: SecurityLVL.Developer,
+            //salary: 55000m,
+            //hireDate: new DateTime(2025, 3, 1),
+            //gender: Gender.F
+            //);
 
-            Console.WriteLine(emp);
+            //Console.WriteLine(emp);
+            #endregion
+
+            #region 2. Develop a Class to represent the Hiring Date Data
+            HiringDate date = new HiringDate(25,6,2021);
+            Console.WriteLine(date.TheDate());
+            #endregion
         }
     }
 }
