40625c0ed79544d2294f2294ade777f687053fa2
fox <textfox10000@gmail.com>
2025-07-14 01:17:40 +0300

    Problem 03

--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -21,9 +21,25 @@ namespace CSharp_Advanced
             #endregion
 
             #region 2. Develop a Class to represent the Hiring Date Data
-            HiringDate date = new HiringDate(25,6,2021);
-            Console.WriteLine(date.TheDate());
+            //HiringDate date = new HiringDate(25,6,2021);
+            //Console.WriteLine(date.TheDate());
             #endregion
+
+            #region 3.Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
+            Employees03[] employees = new Employees03[3];
+            employees[0] = new Employees03(1, "Mohammed", SecurityLVL03.DBA);
+            employees[1] = new Employees03(2, "Ali", SecurityLVL03.Guest);
+            employees[2] = new Employees03(3, "Khaled", SecurityLVL03.Security_Officer);
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+            #endregion
+
+
+
+
         }
     }
 }
